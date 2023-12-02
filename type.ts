e5fe9b8d65277dcb7f7ad199e68a010075942bf6
hsjoihs / sozysozbot <sozysozbot@gmail.com>
2023-12-02 16:27:57 +0900

    nubepok leti dat の語源欄を埋め、型名を微調整

--- type.ts
+++ type.ts
@@ -1,14 +1,14 @@
 export type Tag = "ftlexest" | "kraftanix" | "krackrafi'a" | "kraftan" | "kranti'a" | "fastarkraftona" | "pestarkraftona";
-export type TranslationFKJoined = { title: "farteven ad kante", forms: string[] };
-export type TranslationFKSeparate = { title: "farteven" | "kante", forms: string[] };
+export type EtymologyEqualToMeaning = { title: "farteven ad kante", forms: string[] };
+export type EtymologySeparateFromMeaning = { title: "farteven" | "kante", forms: string[] };
 export type LinzklarTranslation = { title: "linzklar", forms: string[] };
 export type Content = { title: "nefisna" | "harda kraxaiun", text: string };
 
 export type Word = {
   entry: { id: number, form: string },
   translations:
-  (TranslationFKJoined | LinzklarTranslation)[] |
-  (TranslationFKSeparate | LinzklarTranslation)[],
+  (EtymologyEqualToMeaning | LinzklarTranslation)[] |
+  (EtymologySeparateFromMeaning | LinzklarTranslation)[],
   tags: Tag[],
   contents: Content[],
   variations: [],
